e70b0d14ce404b41c48d0327dfb0245d1545706e
Aubrey Jones <aljones2@uw.edu>
2021-03-02 00:03:56 -0800

    updated syntax

--- wa_income_location_chart.R
+++ wa_income_location_chart.R
@@ -18,18 +18,18 @@ income_by_location <- read.csv("income_by_location.csv")
 # Chart 2 ----------------------------------------------------------------------
 
 wa_income_location <- income_by_location %>%
-  filter(`ID Race` >= "1", `ID Race` <= "9") %>%
+  filter(`ID.Race` >= "1", `ID.Race` <= "9") %>%
   mutate("County" = str_sub(Geography, 1, -11)) %>%
-  select(Race, Year, `Household Income by Race`, County)
+  select(Race, Year, `Household.Income.by.Race`, County)
 ggplot(data = wa_income_location) +
-  geom_point(mapping = aes(x = Year, y = `Household Income by Race`,
+  geom_point(mapping = aes(x = Year, y = `Household.Income.by.Race`,
                            color = Race)) +
   labs(title = "Household Income by Race in Washington State Between 2013 and
        2018",
        x = "Time (years)",
        y = "Household Income")
 ggplot(data = wa_income_location) +
-  geom_point(mapping = aes(x = Year, y = `Household Income by Race`,
+  geom_point(mapping = aes(x = Year, y = `Household.Income.by.Race`,
                            color = County)) +
   labs(title = "Household Income by County in Washington State Between 2013 and
        2018",
