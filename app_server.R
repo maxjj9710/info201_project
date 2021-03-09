ae15cf87d295193c3044c7f4440cc9b4b1e90907
Vriana Gatdula <vrimargatdula@gmail.com>
2021-03-09 13:49:07 -0800

    start server file

---
+++ app_server.R
@@ -0,0 +1,9 @@
+# Load the libraries so they are available.
+library(dplyr)
+library(ggplot2)
+library(lintr)
+library(stringr)
+library(tidyverse)
+
+# Load the dataset.
+source("income_by_location.csv")
