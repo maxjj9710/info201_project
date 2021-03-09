8ff0950ac2772d6904eac86e63319f033be07d61
Vriana Gatdula <vrimargatdula@gmail.com>
2021-03-09 13:48:10 -0800

    start app file

---
+++ app.R
@@ -0,0 +1,12 @@
+# Setup ------------------------------------------------------------------------
+
+# Load the libraries so they are available.
+library(shiny)
+
+# Use source() to execute the `app.ui.R` and `app_server.R` files. These will
+# define the UI value and function, respectively.
+source("app_ui.R")
+source("app_server.R")
+
+# Create a new shinyApp() using the loaded `ui` and `server` variables.  
+shinyApp(ui = ui, server = server)
