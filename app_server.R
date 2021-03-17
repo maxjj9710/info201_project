d96ff45cbd78a13afc7b7d1e5b6574bc8843c35e
Vriana Gatdula <vrimargatdula@gmail.com>
2021-03-16 18:25:15 -0700

    add third chart

--- app_server.R
+++ app_server.R
@@ -10,5 +10,19 @@ source("income_by_location.csv")
 
 # Define the server.
 server <- function(input, output){
-  # Insert the code for the charts here. 
+  # Insert the code for the charts here.
+  output$income_location_plot <- renderPlotly({
+    wa_income_location <- filter(year >= input$date_range[1],
+             year <= input$date_range[2]) %>%
+      group_by()
+    income_location_chart <- ggplot(data = wa_income_location) +
+      geom_point(mapping = aes(x = Year, y = Household.Income.by.Race,
+                               color = County)) +
+      labs(
+        title = "Washington State Income by Race",
+        x = "Time (years)",
+        y = "Household Income",
+        color = "Race"
+      )
+  })
 }
